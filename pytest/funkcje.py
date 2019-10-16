85b40e570abe244df9a313a2645bacdcf46de01f
jedrek1993 <jedrek1993@gmail.com>
2019-10-16 15:47:17 +0200

    Added pytest module

---
+++ pytest/funkcje.py
@@ -0,0 +1,7 @@
+def my_func(x):
+    return x // 2 * 2
+
+
+def bar():
+    l = input("Podaj liczbę: ")
+    return int(l)*2
---
+++ pytest/test_example.py
@@ -0,0 +1,4 @@
+import pytest
+from mock import patch
+
+from funkcje import my_func, bar
